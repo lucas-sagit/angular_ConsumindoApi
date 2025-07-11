055f7d51fab0f4d6aa0a9a4d3dbefc93c977e75a
lucas <lv011297@gmail.com>
2025-07-11 14:12:23 -0300

    add router

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -2,10 +2,12 @@ import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { ProductComponent } from './produts/product.component';
 import { Home } from './home/home';
+import { Pdv } from './pdv/pdv';
 
 export const routes: Routes = [
   { path: 'product', component: ProductComponent },
-  { path: '', component: Home, }
+  { path: '', component: Home, },
+  { path: 'pdv', component: Pdv, }
 ];
 
 @NgModule({
