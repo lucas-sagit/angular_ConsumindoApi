d91695aad546b1a69ad3d75b9a45182064b370d9
lucas <lv011297@gmail.com>
2025-07-11 14:11:51 -0300

    add component

---
+++ src/app/pdv/pdv.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+import { RouterModule } from '@angular/router';
+
+@Component({
+  selector: 'app-pdv',
+  imports: [RouterModule],
+  templateUrl: './pdv.html',
+  styleUrl: './pdv.scss'
+})
+export class Pdv {
+
+}
